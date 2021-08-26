63c66d526d0940a562f173245db7c2bec504f2ab
Murilo Barreto <barreto_11almeida@hotmail.com>
2021-08-26 17:29:52 -0300

    add operations personapi

--- src/main/java/com/demo/personapi/repository/PersonRespository.java
+++
@@ -1,8 +0,0 @@
-package com.demo.personapi.repository;
-
-import com.demo.personapi.entity.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRespository extends JpaRepository<Person, Long> {
-
-}
